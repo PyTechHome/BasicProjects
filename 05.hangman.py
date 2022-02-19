e192694cbea8e40de452f84b66f661f72ec6b4db
wigugu <the_call_of_satan@yahoo.com>
2022-02-19 10:26:36 +0330

    Project 05. Author KH. Date 2022/02/19. Time 10:26 AM.

--- 05.hangman.py
+++ 05.hangman.py
@@ -6,7 +6,7 @@ def get_valid_word(words):
     word=random.choice(words) #randomly chooses somthing from the list
     while '-' in word or ' ' in word:
         word=random.choice(words)
-    return word
+    return word.upper()
 
 def hangman():
     word=get_valid_word(words)
