97155b622eec97da75d6a5f2019445df9322dc90
wigugu <the_call_of_satan@yahoo.com>
2022-02-19 13:48:26 +0330

    Project 05. Author KH. Date 2022/02/19. Time 1:48 PM.

--- 05.hangman.py
+++ 05.hangman.py
@@ -20,7 +20,7 @@ def hangman():
     while len(word_letters) > 0 and lives > 0:
         #Letters used
         # ' '.join(['a','b','cd]) --> 'a b cd'
-        print("You have", lives, " lives left you have used these letters: ", ' '.join(used_letters))
+        print("You have", lives, " lives left. you have used these letters: ", ' '.join(used_letters))
 
         # what the current word is (ie W-R D)
         word_list = [letter if letter in used_letters else '_' for letter in word]
@@ -45,6 +45,6 @@ def hangman():
     if lives==0:
         print("You died, sorry, The word was", word)
     else:
-        print("ou guesses the word", word, "!!")
+        print("Congradulations, You guessed the word", word, "!!")
 
 hangman()
